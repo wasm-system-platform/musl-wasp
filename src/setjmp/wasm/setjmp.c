dec7f06c6d9c44ef31c5c069d0fa8161ffc230a9
Johannes Czekay <johannes.czekay@gmail.com>
2026-05-19 23:43:15 +0200

    dont crash if setjmp does not succeed

--- src/setjmp/wasm/setjmp.c
+++ src/setjmp/wasm/setjmp.c
@@ -10,27 +10,22 @@ int __env_save(jmp_buf env, unsigned long env_size)
 int __env_save_ret;
 
 int setjmp(jmp_buf env) {
-    int save_ret;
-
     __env_save_ret = 0;
 
     __asm__ __volatile__(
         "global.get __stack_pointer\n"
 
+        "local.get %0\n"
         "local.get %1\n"
-        "local.get %2\n"
-		"local.get %3\n"
+		"local.get %2\n"
         "call_indirect (i32, i32) -> (i32)\n"
-        "local.set %0\n"
+        "drop\n"
 
         "global.set __stack_pointer\n"
-        : "=r"(save_ret)
+        :
         : "r"(env->__jb), "r"(sizeof(__jmp_buf)), "r"(__env_save)
         : "memory"
     );
-
-    if (save_ret != 0)
-        __builtin_trap();
 
     return __env_save_ret;
 }
